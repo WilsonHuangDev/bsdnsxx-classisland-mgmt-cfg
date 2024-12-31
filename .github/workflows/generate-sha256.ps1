b2f5f86c9a2581b798cd89f0bf8240543e64d356
Wilson.Huang <114924470+WilsonHuangDev@users.noreply.github.com>
2024-12-31 11:29:41 +0800

    Update generate-sha256.ps1
    Signed-off-by: Wilson.Huang <114924470+WilsonHuangDev@users.noreply.github.com>

--- .github/workflows/generate-sha256.ps1
+++ .github/workflows/generate-sha256.ps1
@@ -5,6 +5,18 @@ rm $path/*.sha-256
 $files = Get-ChildItem $path
 $hashes = [ordered]@{}
 $summary = "
+新增功能与优化
+------
+- 
+
+Bug 修复
+------
+- 
+<br>
+
+**Full Changelog**: https://github.com/WilsonHuangDev/bsdnsxx-classisland-mgmt-cfg/compare/
+
+------
 > [!important]
 > 下载时请注意核对文件SHA-256是否正确。
 
